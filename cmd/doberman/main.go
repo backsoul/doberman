5619b926b9d8bdf5816a31426a22661dd1d9d824
daniel.sarmiento3 <daniel.sarmiento@dafiti.com.co>
2022-06-09 13:23:18 -0500

    implement log service basic

--- cmd/doberman/main.go
+++ cmd/doberman/main.go
@@ -5,5 +5,5 @@ import (
 )
 
 func main() {
-	services.Log("HI")
+	services.Log("HI", "")
 }
--- pkg/services/log_service.go
+++ pkg/services/log_service.go
@@ -1,13 +1,25 @@
-package main
+package services
 
 import (
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 )
 
-func Log(message string, args ...interface{}) {
+func Log(message string, typeMsg string, args ...interface{}) {
 	// UNIX Time is faster and smaller than most timestamps
 	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
 
-	log.Print(message)
+	switch typeMsg {
+	case "Info":
+		log.Debug().
+			Str("message", message).Send()
+	case "Error":
+		log.Error().
+			Str("message", message).Send()
+	default:
+		log.Debug().
+			Str("message", message).Send()
+
+	}
+
 }
